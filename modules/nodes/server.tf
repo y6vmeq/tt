cd6825a760cb5bf888acb1f2e22828cad159c14c
Aliaksandr Padrabinkin <y6vmeq@gmail.com>
2019-10-08 12:04:57 +0300

    EOF

--- modules/nodes/server.tf
+++ modules/nodes/server.tf
@@ -6,12 +6,13 @@ resource "google_compute_instance_from_template" "default_server" {
 }
 resource "null_resource" "provision_server" {
   provisioner "remote-exec" {
-    inline = [
-      "wget https://releases.hashicorp.com/vault/1.2.3/vault_1.2.3_linux_amd64.zip",
-      "sudo unzip vault_1.2.3_linux_amd64.zip -d /usr/local/bin/",
-      "sudo rm -rf vault_1.2.3_linux_amd64.zip",
-      "sudo chmod +x /usr/local/bin/vault",
-      "vault server -dev -dev-listen-address=0.0.0.0:8200 &",
+    inline = [<<EOF
+      wget https://releases.hashicorp.com/vault/1.2.3/vault_1.2.3_linux_amd64.zip
+      sudo unzip vault_1.2.3_linux_amd64.zip -d /usr/local/bin/
+      sudo rm -rf vault_1.2.3_linux_amd64.zip
+      sudo chmod +x /usr/local/bin/vault
+      vault server -dev -dev-listen-address=0.0.0.0:8200 &
+      EOF
     ]
   }
   connection {
