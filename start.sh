60dfa6e6ddf9663bb0681aa61b7438e92d46756f
Aliaksandr Padrabinkin <46869350+y6vmeq@users.noreply.github.com>
2019-10-08 11:02:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@ echo "############## ===== Install  Terraform ===== ##############"
 echo "##############                                ##############"
 echo "############################################################"
 sudo wget -c --progress=bar https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip
-sudo unzip terraform_0.12.9_linux_amd64.zip -d /usr/bin/
+sudo unzip -o terraform_0.12.9_linux_amd64.zip -d /usr/bin/
 sudo chmod +x /usr/bin/terraform
 sudo rm -rf terraform_0.12.9_linux_amd64.zip
 echo "############################################################"
